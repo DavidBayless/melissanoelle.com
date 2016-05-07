2414cf7f1cc95351c2fdaf567cf7c91c0f60b566
Melissa Noelle <melissamatara@gmail.com>
2016-05-06 21:53:23 -0400

    Add out base pages so all the links work

--- server.rb
+++ server.rb
@@ -5,3 +5,19 @@ require 'haml'
 get '/', provides: :html do
   haml :blog
 end
+
+get '/events', provides: :html do
+  haml :events_speaking
+end
+
+get '/slides', provides: :html do
+  haml :slides
+end
+
+get '/work', provides: :html do
+  haml :work
+end
+
+get '/contact', provides: :html do
+  haml :contact_me
+end
