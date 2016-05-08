68eabbef1d0df94b1337e51bebca87ee47dac6c6
Melissa Noelle <melissamatara@gmail.com>
2016-05-07 20:24:21 -0400

    Add sass and markdown support

--- server.rb
+++ server.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'sass'
+require 'sass/plugin/rack'
 require 'haml'
+require 'date'
+require 'redcarpet'
+
+Sass::Plugin.options[:style] = :compressed
+use Sass::Plugin::Rack
 
 get '/', provides: :html do
   haml :blog
@@ -20,4 +26,10 @@ end
 
 get '/contact', provides: :html do
   haml :contact_me
+end
+
+# posts
+
+get '/starting-with-git', provides: :html do
+  haml :'posts/git_intro'
 end
